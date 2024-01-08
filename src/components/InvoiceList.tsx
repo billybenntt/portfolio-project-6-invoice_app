6dd590a144bb4bd3d5494c1d363d924d56b678af
billbenntt@gapp.nthu.edu.tw <billbenntt@gapp.nthu.edu.tw>
2024-01-08 17:36:44 +0800

    worked on Invoice List

--- src/components/InvoiceList.tsx
+++ src/components/InvoiceList.tsx
@@ -1,9 +1,58 @@
+import data from "../data/sampleAllData.tsx";
+import InvoiceListItem from "./InvoiceListItem.tsx";
+
 function InvoiceList() {
-  return (
-    <>
-      <h2>InvoiceList Component</h2>
-    </>
-  )
+
+    const invoices = data
+    const showInvoices = invoices.length > 0
+
+    const invoiceListItems = invoices.map((item) => {
+        return <InvoiceListItem key={item.id} {...item}/>
+    })
+
+    return (
+        <>
+            <div className="invoice-list__header">
+                <div>
+                    <h1 className="invoice-list__title">Invoices</h1>
+                    <p className="invoice-list__amount">
+                        There are 7 total invoices
+                    </p>
+
+                </div>
+
+                <div className="invoice-list__filter">
+                    <h5 className="">Filter by status</h5>
+                    <span>🔽</span>
+                </div>
+
+                <div className="invoice-list__create">
+                    <button className="btn-primary large">
+                    <span className="button-icon-container">
+                    <span>➕</span>
+                    </span>
+                        <span className="desktop">New Invoice</span>
+                        <span className="mobile">New</span>
+                    </button>
+                </div>
+            </div>
+
+            {showInvoices ? (
+                <ul className="invoice-list__container" style={{border: "1px solid black"}}>
+                    {invoiceListItems}
+                </ul>
+            ) : (
+                <div className="invoice-list__none">
+                    <button>empty illustration</button>
+                    <h2 className="invoice-list__none-title">There is nothing here</h2>
+                    <p>
+                        Create an invoice by clicking the{" "}
+                        <span>New Invoice</span> button and get started
+                    </p>
+                </div>
+            )}
+        </>
+    );
 }
 
-export default InvoiceList
+export default InvoiceList;
---
+++ src/components/InvoiceListItem.tsx
@@ -0,0 +1,57 @@
+type Invoice = {
+    id: string,
+    paymentDue: string,
+    clientName: string,
+    total: number,
+    status: string
+}
+
+function InvoiceListItem(props: Invoice) {
+
+    const {id, paymentDue, clientName, total, status} = props
+
+    return (
+        <li className="invoice-list__item"
+            tabIndex={0}
+            key={id}
+            onClick={() => console.log(1)}>
+            <div className="invoice-list__item-value">
+                <span className="hash">#</span>
+                {id}
+            </div>
+            <div className="invoice-list__item-value">
+                <span>Due </span> {paymentDue}
+            </div>
+            <div className="invoice-list__item-value">
+                {clientName}
+            </div>
+
+            <div className="invoice-list__item-value">
+                  <span>
+                    <span>Due</span>{" "}
+                      {paymentDue}
+                  </span>
+                <span>
+                    NTD{total}
+                  </span>
+            </div>
+
+            <div className="invoice-list__item-value desktop">
+                <span>NTD</span>
+                {total}
+            </div>
+
+            <div className={"invoice-list__item-value invoice-status " + status}>
+                <div className="status-container">
+                    <span className="status-point"/>
+                    <span className="status-text">{status}</span>
+                </div>
+            </div>
+            <span className="invoice-list__item-value">
+                <span>▶️</span>
+            </span>
+        </li>
+    )
+}
+
+export default InvoiceListItem
